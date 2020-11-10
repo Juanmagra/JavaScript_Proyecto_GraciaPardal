f9356a7e74db3ef600a2039b85010a860343affc
Juan Manuel Gracia Pardal <gracia.pajua20@aulas.triana>
2020-11-10 14:25:24 +0100

    Retoque de clases y estructura

---
+++ ParkingControll_GraciaPardal/src/Datos.js
@@ -0,0 +1,9 @@
+import { Vehiculo, TIPO, Cliente, Tipo, Plaza,Ticket, Parking, } from "./Models.js";
+
+let plazaMoto = new Tipo (TIPO.MOTO, 5);
+let plazaCarabana = new Tipo(TIPO.CARABANA, 2);
+LET plazaTurismo = new Tipo(TIPO.TURISMO, 10);
+
+
+
+
---
+++ ParkingControll_GraciaPardal/src/Models.js
@@ -0,0 +1,56 @@
+
+export const TIPO = Object.freeze({
+    MOTO:   "MOTO",
+    TURISMO:  "TURISMO",
+    CARABANA: "CARABANA"
+});
+
+export class Tipo{
+    constructor(nombre, cantida){
+        this.nombre = nombre;
+        this.cantida = cantida;
+    }
+}
+export class Vehiculo{
+    constructor(matricula, Tipo) {
+        this.matricula = matricula;
+        this.Tipo = Tipo;
+      }
+}
+
+export class Plaza{
+    constructor(id, tipo, estado){
+        this.id=id;
+        this.tipo = tipo;
+        this.estado = estado;
+    }   
+}
+export class Parking{
+    constructor(Plaza, clientes){
+        this.Plaza = Plaza;
+        this.clientes = clientes;
+    }
+}
+export class Ticket{
+    constructor(id,matricula, entrada, salida, pin, plazaId){
+        this.id = id;
+        this.matricula=matricula;
+        this.entrada = entrada;
+        this.salida = salida;
+        this.pin = pin;
+        this.plazaId = plazaId;
+    }
+}
+export class Cliente{
+    constructor(nombre, dni, abonoPin, vehiculo, plazaId){
+        this.nombre = nombre;
+        this.dni = dni;
+        this.abonoPin = abonoPin;
+        this.vehiculo = vehiculo;
+        this.plazaId = plazaId;
+    }
+}
+
+
+
+
